9162ac24e265570477ad8ab5f1fd466a8c18731f
iwanaga0225 <n.iwanaga.amethyst@gmail.com>
2024-01-05 20:37:26 +0900

    マイグレーションに制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -63,9 +63,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_25_200014) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
-    t.text "profile"
-    t.text "occupation"
-    t.text "position"
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
